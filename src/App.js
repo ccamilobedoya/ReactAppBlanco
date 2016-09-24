4c91acfa190595e6d252690ce5453d9c22e505d1
ccamilobedoya <ccamilo.bedoya@gmail.com>
2016-09-24 08:39:17 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+var Hola = React.createClass({
+  
+  render: function() {
+    return (
+   	<label>hola mundo</label>
+    );
+  }
+});
+
+export default Hola;
